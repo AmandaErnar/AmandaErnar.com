a7d3cf23229147654d15090b472ed996ead89baf
AmandaErnar <amandaernar@icloud.com>
2023-02-04 12:03:53 -0700

    styles??

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,6 @@
-<link rel="stylesheet" href="https://use.typekit.net/xlg6gju.css">
-
-<script>
-    import "$lib/styles/style.css";
-</script>
+<svelte:head>
+    <link rel="stylesheet" href="https://use.typekit.net/xlg6gju.css">
+</svelte:head>
 
 <header>
     <a href="/">
@@ -26,4 +24,49 @@
 </main>
 
 <footer>&copy; Amanda Ernar 2023</footer>
+
+<style>
+    :global(body) {
+        margin: 0;
+        color: white;
+        background: #a7b7d7;
+        font-family: century-old-style-std, serif;
+        padding: 1rem;
+    }
+
+    main {
+        margin: 2rem auto;
+        max-width: 40rem;
+        padding: 2rem;
+    }
+
+    header {
+        padding: 2rem;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+
+    ul {
+        margin: 0;
+        list-style-type: none;
+        display: flex;
+        gap: 1rem;
+    }
+
+    a {
+        text-decoration: none;
+        font-style: italic;
+        color: inherit;
+    }
+
+    footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        padding-bottom: 2rem;
+        text-align: center; 
+    }
+</style>
 
