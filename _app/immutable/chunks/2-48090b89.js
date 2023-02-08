a7e192cb5ac3280cc8c9b189bae67ba9ee2b5ae8
AmandaErnar <amandaernar@icloud.com>
2023-02-07 20:20:21 -0700

    Updates

--- _app/immutable/chunks/2-48090b89.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-00b3a566.js";export{t as component};
---
+++ _app/immutable/chunks/2-a1abba3e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-e1487678.js";export{t as component};
